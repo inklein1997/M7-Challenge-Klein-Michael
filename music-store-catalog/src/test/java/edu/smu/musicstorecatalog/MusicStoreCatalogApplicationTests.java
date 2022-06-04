3e8ce632a4a0548d1560a1c4ad99aa1fa34bcb3d
Michael Klein <Michaelklein1997@gmail.com>
2022-06-03 21:16:58 -0500

    initial commit

---
+++ music-store-catalog/src/test/java/edu/smu/musicstorecatalog/MusicStoreCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.smu.musicstorecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ music-store-recommendations/src/main/java/edu/smu/musicstorerecommendations/MusicStoreRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package edu.smu.musicstorerecommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreRecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreRecommendationsApplication.class, args);
+	}
+
+}
