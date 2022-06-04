d6fa132bd56673b236ddd253f2fd850a1d513c70
Michael Klein <Michaelklein1997@gmail.com>
2022-06-03 21:28:14 -0500

    created Artist DTO

---
+++ music-store-catalog/src/main/java/edu/smu/musicstorecatalog/dto/Artist.java
@@ -0,0 +1,102 @@
+package edu.smu.musicstorecatalog.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "artist")
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "artist_id")
+    private int artistId;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @Null
+    @Size(max = 255)
+    private String instagram;
+
+    @Null
+    @Size(max = 255)
+    private String twitter;
+
+    public Artist(int artistId, String name, String instagram, String twitter) {
+        this.artistId = artistId;
+        this.name = name;
+        this.instagram = instagram;
+        this.twitter = twitter;
+    }
+
+    public Artist(String name, String instagram, String twitter) {
+        this.name = name;
+        this.instagram = instagram;
+        this.twitter = twitter;
+    }
+
+    public Artist() {
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return artistId == artist.artistId && Objects.equals(name, artist.name) && Objects.equals(instagram, artist.instagram) && Objects.equals(twitter, artist.twitter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artistId, name, instagram, twitter);
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", name='" + name + '\'' +
+                ", instagram='" + instagram + '\'' +
+                ", twitter='" + twitter + '\'' +
+                '}';
+    }
+}
